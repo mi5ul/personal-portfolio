9b80c360d1305a6e0ecc11df160a4f76ffe68532
mi5ul <mirt.suljic@gmail.com>
2018-03-26 15:41:03 +1300

    Implemented the typing animation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Does not work for some reason
 // Can also be included with a regular script tag
-import Typed from 'typed.js';
+import Typed from "typed.js";
 
 var options = {
   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
