7d510b7c7f4a402f1fddf902f5039c5efc275390
mi5ul <mirt.suljic@gmail.com>
2018-04-08 22:02:53 +1200

    added landing page

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
-import { name, btn } from './test.js';
+// import { name, btn } from './test.js';
 import Typed from 'typed.js';
 
 var options = {
-  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+  strings: ["Hello, my name is Mirt", "I am a Web Developer"],
   typeSpeed: 40
 }
 
@@ -10,20 +10,20 @@ var typed = new Typed("#typed", options);
 
 
 
-const displayName = () => {
-	console.log('Works fine!')
-	let nameNode = document.createTextNode('My name is Mirt Suljic.');
-	name.appendChild(nameNode);
-}
+// const displayName = () => {
+// 	console.log('Works fine!')
+// 	let nameNode = document.createTextNode('My name is Mirt Suljic.');
+// 	name.appendChild(nameNode);
+// }
 
-const changeColour = () => {
-	if (name.style.color === "" || name.style.color === "black") {
-		name.style.color = "purple";
-	} else {
-		name.style.color = "black";
-	}
-}
+// const changeColour = () => {
+// 	if (name.style.color === "" || name.style.color === "black") {
+// 		name.style.color = "purple";
+// 	} else {
+// 		name.style.color = "black";
+// 	}
+// }
 
-btn.addEventListener('click', changeColour);
+// btn.addEventListener('click', changeColour);
 
-displayName();
+// displayName();
