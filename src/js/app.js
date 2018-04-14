6f52a272897b011182a53c35f9e040d1c3d4582d
mi5ul <mirt.suljic@gmail.com>
2018-04-14 21:46:35 +1200

    added progressbar.js

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,20 @@ var bar = new ProgressBar.Circle(container, {
   easing: 'easeInOut',
   duration: 1400,
   text: {
+    value: '<i class="fab fa-js"></i>',
+    style: {
+      color: '#1B213B',
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: 0,
+      margin: 0,
+      // You can specify styles which will be browser prefixed
+      transform: {
+        prefix: true,
+        value: 'translate(-50%, -50%)'
+      }
+    },
     autoStyleContainer: false
   },
   from: { color: '#aaa', width: 1 },
@@ -29,11 +43,12 @@ var bar = new ProgressBar.Circle(container, {
     circle.path.setAttribute('stroke-width', state.width);
 
     var value = Math.round(circle.value() * 100);
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(value);
-    }
+
+    // if (value === 0) {
+    //   circle.setText('');
+    // } else {
+    //   circle.setText(value);
+    // }
 
   }
 });
