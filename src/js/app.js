b1397a3e68f3e6285bd7091b376d7dc5bbc7152c
mi5ul <mirt.suljic@gmail.com>
2018-04-15 23:16:11 +1200

    added progress circles

--- src/js/app.js
+++ src/js/app.js
@@ -10,49 +10,50 @@ var options = {
 var typed = new Typed("#typed", options);
 
 // Progress bars animation
-var bar = new ProgressBar.Circle(container, {
-  color: '#aaa',
-  // This has to be the same size as the maximum width to
-  // prevent clipping
-  strokeWidth: 4,
-  trailWidth: 1,
-  easing: 'easeInOut',
-  duration: 1400,
-  text: {
-    value: '<i class="fab fa-js"></i>',
-    style: {
-      color: '#1B213B',
-      position: 'absolute',
-      left: '50%',
-      top: '50%',
-      padding: 0,
-      margin: 0,
-      // You can specify styles which will be browser prefixed
-      transform: {
-        prefix: true,
-        value: 'translate(-50%, -50%)'
-      }
+const animateProgressBar = (container, pathPercent) => {
+  // progressbar.js@1.0.0 version is used
+  // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+  var bar = new ProgressBar.Circle(container, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+      autoStyleContainer: false
     },
-    autoStyleContainer: false
-  },
-  from: { color: '#aaa', width: 1 },
-  to: { color: '#333', width: 4 },
-  // Set default step function for all animate calls
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#333', width: 4 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
 
-    var value = Math.round(circle.value() * 100);
+      var value = Math.round(circle.value() * 100);
+      if (value === 0) {
+        circle.setText('');
+      } else {
+        circle.setText(value);
+      }
 
-    // if (value === 0) {
-    //   circle.setText('');
-    // } else {
-    //   circle.setText(value);
-    // }
+    }
+  });
+  bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+  bar.text.style.fontSize = '2rem';
 
-  }
-});
-bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-bar.text.style.fontSize = '2rem';
+  bar.animate(pathPercent);  // Number from 0.0 to 1.0
+}
 
-bar.animate(1.0);  // Number from 0.0 to 1.0
+// Select all container to animate the progressbar
+const progressbarContainers = [
+  ['#container1', 0.8],
+  ['#container2', 0.7],
+  ['#container3', 0.5]
+];
+
+for (let cont of progressbarContainers) {
+  animateProgressBar(cont[0], cont[1]);
+}
